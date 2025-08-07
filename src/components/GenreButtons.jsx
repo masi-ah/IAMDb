11bf618cb02ececbdd67deae19c672941911ee2d
Masi <mas.ahmadii@yahoo.com>
2025-08-07 11:27:01 +0330

    feat: add navigation on genre button click

--- src/components/GenreButtons.jsx
+++ src/components/GenreButtons.jsx
@@ -1,9 +1,16 @@
 import GenreButton from "./GenreButton";
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const GenreButtons = () => {
   const genres = ["Crime", "Drama", "Action", "Biography"];
   const [selectedGenre, setSelectedGenre] = useState(null);
+  const navigate = useNavigate();
+   
+  const handleClick = (genre) => {
+    setSelectedGenre(genre);
+    navigate(`/list/${genre.toLowerCase()}`);
+  };
 
   return (
     <div className="flex flex-wrap gap-[10px] justify-center mt-[30px]">
@@ -12,7 +19,7 @@ const GenreButtons = () => {
         key={genre} 
         genre={genre}
         isActive={selectedGenre === genre}
-        onClick={() => setSelectedGenre(genre)}
+        onClick={() => handleClick(genre)}
         />
       ))}
     </div>
