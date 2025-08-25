e2a8efd9ab0e3ad9bb199a12dfcbcf76279cd9c4
Masi <mas.ahmadii@yahoo.com>
2025-08-25 09:56:40 +0330

    feat: add FavoritesContext for like state management

---
+++ src/contexts/FavoritesContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState, useEffect, Children } from 'react';
+
+const FavoritesContext = createContext ();
+
+const FavoritesProvider = ({ children}) => {
+  const [favorites, setFavorites] = useState(() => {
+   const saved = localStorage.getItem(`favorites`);
+   return saved ? new Set(JSON.parse(saved)) : new Set();
+  });
+
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify([...favorites]));
+  }, [favorites]);
+
+  const switchFavorite = (movieId) => {
+    setFavorites(prev => {
+      const newFavorites = new Set(prev);
+      if (newFavorites.has(movieId)) {
+        newFavorites.delete(movieId);
+      } else {
+        newFavorites.add(movieId);
+      }
+      return newFavorites;
+    });
+  };
+
+  const isFavorite = (movieId) => favorites.has(movieId);
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, switchFavorite, isFavorite}}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}; 
+
+const useFavorites = () => {
+  return useContext(FavoritesContext)
+  };
+
+  export { FavoritesProvider, useFavorites };
+  export default FavoritesContext;
